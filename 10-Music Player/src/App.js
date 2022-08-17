6bf1d1834ec9b7dc55aeb031c224edfc6c684a95
sdwoo <damjang67@naver.com>
2022-08-18 02:18:19 +0900

    feat:rewind-10-music-player

---
+++ 10-Music Player/src/App.js
@@ -0,0 +1,86 @@
+import MusicInfo from './component/MusicInfo.js';
+import Audio from './component/Audio.js';
+import ImgContainer from './component/ImageContainer.js';
+import Navigation from './component/Navigation.js';
+
+const titleList = ['hey', 'summer', 'ukulele'];
+
+export default class App {
+  constructor($app) {
+    this.state = {
+      currentIdx: 0,
+      play: false,
+    };
+    this.$target = $app;
+    this.onClick = () => {
+      this.$target.classList.add('play');
+      this.setState({
+        ...this.state,
+        currentIdx:
+          this.state.currentIdx + 1 >= titleList.length
+            ? 0
+            : this.state.currentIdx + 1,
+        play: true,
+      });
+    };
+
+    this.musicInfo = new MusicInfo({
+      $app,
+      initialState: titleList[this.state.currentIdx],
+      onClickProgress: (clickPercent) => {
+        this.audio?.setCurrentTime(clickPercent);
+      },
+    });
+
+    this.audio = new Audio({
+      $app,
+      initialState: {
+        music: titleList[this.state.currentIdx],
+        play: this.state.play,
+      },
+      handleProgress: (width) => {
+        this.musicInfo.progress(width);
+      },
+      onClickNext: this.onClick,
+    });
+
+    this.imgContainer = new ImgContainer({
+      $app,
+      initialState: titleList[this.state.currentIdx],
+    });
+
+    this.navigation = new Navigation({
+      $app,
+      initialState: this.state.play,
+      onClickPrev: () => {
+        this.$target.classList.add('play');
+        this.setState({
+          ...this.state,
+          currentIdx:
+            this.state.currentIdx - 1 < 0
+              ? titleList.length - 1
+              : this.state.currentIdx - 1,
+          play: true,
+        });
+      },
+      onClickPlay: () => {
+        this.$target.classList.toggle('play');
+        this.setState({
+          ...this.state,
+          play: !this.state.play,
+        });
+      },
+      onClickNext: this.onClick,
+    });
+  }
+  setState(nextState) {
+    this.state = nextState;
+    this.musicInfo.setState(titleList[this.state.currentIdx]);
+    this.audio.setState({
+      music: titleList[this.state.currentIdx],
+      play: this.state.play,
+    });
+    this.imgContainer.setState(titleList[this.state.currentIdx]);
+    this.navigation.setState(this.state.play);
+  }
+}
